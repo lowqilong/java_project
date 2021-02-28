ecff720c9d2a0b564f9fcff479a2c7f208fb871c
lowqilong <lowqilong@hotmail.com>
2021-02-28 20:15:13 +0800

    reinitialising

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package game.pokemon;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PokemonApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PokemonApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package game.pokemon;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PokemonApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
